4aa33a8f864afaf02e3dfaadc1610c03bfc6e35e
Steven Martin <steven@goteleport.com>
2022-02-05 14:03:59 -0500

    updates to have all commands

--- tsh/tshcomplete.zsh
+++ tsh/tshcomplete.zsh
@@ -38,11 +38,13 @@ _tsh() {
                     "play[Replay the recorded SSH session]" \
                     "aws[Access AWS API]" \
                     "apps[Application functions ls login logout config]" \
+                    "app[Application functions ls login logout config]" \
                     "proxy[Local proxy for ssh or db]" \
                     "scp[Secure file copy]" \
                     "status[Display the list of proxy servers and retrieved certificates]" \
                     "env[Print commands to set Teleport session environment variables]" \
                     "request[Access Request functions]" \
+                    "requests[Access Request functions]" \
                     "kube[Kubernetes functions]" \
                     "mfa[MFA functions]" \
                     "config[Print OpenSSH configuration details]" \
@@ -61,14 +63,56 @@ _tsh() {
                 login)
                    _login_tsh_cmd
                     ;;
+                apps)
+                   _apps_tsh_cmd
+                    ;;
+                app)
+                   _apps_tsh_cmd
+                    ;;
+                kube)
+                   _kube_tsh_cmd
+                    ;;
+                proxy)
+                   _proxy_tsh_cmd
+                    ;;
+                mfa)
+                   _mfa_tsh_cmd
+                    ;;
+                requests)
+                   _requests_tsh_cmd
+                    ;;
+                request)
+                   _requests_tsh_cmd
+                    ;;
+                scp)
+                   _scp_tsh_cmd
+                    ;;
+# will need to make one that doesn't fail on arguments
+                help)
+                   _tsh
+                    ;;
             esac
             ;;
     esac
 }
 
+_scp_tsh_cmd() {
+    local line state
+
+    _arguments -s \
+  "-r[Recursive copy of subdirectories]" \
+  "--recursive[Recursive copy of subdirectories]" \
+  "-P[Port to connect to on the remote host]" \
+  "--port[Port to connect to on the remote host]" \
+  "-p[Preserves access and modification times from the original file]" \
+  "--preserve[Preserves access and modification times from the original file]" \
+  "-q[Quiet mode]" \
+  "--quiet[Quiet mode]"
+}
+
 _login_tsh_cmd() {
-  _arguments "--login[Remote host login]" \
-             "--proxy[SSH proxy address]" \
+  _arguments -s "(--login)--login[Remote host login]" \
+             "(--proxy)--proxy[SSH proxy address]" \
              "--user[SSH proxy user]" \
              "--ttl[Minutes to live for a SSH session]" \
              "-i[Identity file]" \
@@ -96,6 +140,137 @@ _login_tsh_cmd() {
              "--request-id[Login with the roles requested in the given request]" \
              "--browser[Set to none to suppress browser opening on login]" \
              "--kube-cluster[Name of the Kubernetes cluster to login to]"
+}
+
+_requests_tsh_cmd() {
+    local line state
+
+    _arguments -C \
+               "1: :->cmds" \
+               "*::arg:->args"
+
+    case "$state" in
+        cmds)
+            _values "requests" \
+                    "ls[List access requests]" \
+                    "list[List access requests]" \
+                    "show[Show request details]" \
+                    "details[Show request details]" \
+                    "new[Create a new access request]" \
+                    "create[Create a new access request]" \
+                    "review[Review an access request]"
+            ;;
+        args)
+            case $line[1] in
+                new)
+                    _new_requests_tsh_cmd
+                    ;;
+                create)
+                    _new_requests_tsh_cmd
+                    ;;
+                review)
+                    _review_requests_tsh_cmd
+                    ;;
+            esac
+            ;;
+    esac
+}
+
+_new_requests_tsh_cmd() {
+    local line state
+
+    _arguments -s \
+               "--roles[Roles to be requested]" \
+      "--reason[Reason for requesting]" \
+      "--reviewers[Suggested reviewers]" \
+      "--nowait[Finish without waiting for request resolution]"
+}
+
+_review_requests_tsh_cmd() {
+    local line state
+
+    _arguments -s \
+             "--approve[Review proposes approval]" \
+             "--deny[Review proposes denial]" \
+             "--reason[Review reason message]"
+}
+
+_mfa_tsh_cmd() {
+    local line state
+
+    _arguments -C \
+               "1: :->cmds"
+
+    case "$state" in
+        cmds)
+            _values "mfa" \
+                "ls[Get a list of registered MFA devices]" \
+                "add[Add a new MFA device]" \
+                "rm[Remove a MFA device]"
+            ;;
+    esac
+
+}
+
+_proxy_tsh_cmd() {
+    local line state
+
+    _arguments -C \
+               "1: :->cmds"
+
+    case "$state" in
+        cmds)
+            _values "proxy" \
+                "ssh[Start local TLS proxy for ssh connections when using Teleport in single\-port mode]" \
+                "db[Start local TLS proxy for database connections when using Teleport in single\-port mode]"
+            ;;
+    esac
+
+}
+
+_kube_tsh_cmd() {
+    local line state
+
+    _arguments -C \
+               "1: :->cmds" 
+
+    case "$state" in
+        cmds)
+            _values "kube" \
+                    "ls[Get a list of kubernetes clusters]" \
+                    "login[Login to a kubernetes cluster]" 
+            ;;
+    esac
+}
+
+_apps_tsh_cmd() {
+    local line state
+
+    _arguments -C \
+               "1: :->cmds" \
+               "*::arg:->args"
+
+    case "$state" in
+        cmds)
+            _values "apps" \
+                    "ls[List available applications]" \
+                    "login[Retrieve short-lived certificate for an app.]" \
+                    "logout[Remove app certificate.]" \
+                    "config[Print app connection information.]"
+            ;;
+        args)
+            case $line[1] in
+                login)
+                    _apps_login_tsh_cmd
+                    ;;
+            esac
+            ;;
+    esac
+}
+
+_apps_login_tsh_cmd() {
+    _arguments -s \
+               '--aws-role[(For AWS CLI access only) Amazon IAM role ARN or role name.]'
 }
 
 _db_tsh_cmd() {
