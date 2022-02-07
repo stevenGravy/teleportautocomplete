88458b8d4ffb9c8b96d9c87857484652b064ac3e
Steven <steven@goteleport.com>
2022-02-06 22:22:44 -0500

    Implementing tctl

--- tctl/tctlcomplete.zsh
+++ tctl/tctlcomplete.zsh
@@ -38,13 +38,92 @@ _tctl() {
                 users)
                     _users_tctl_cmd
                     ;;
-# will need to make one that doesn't fail on arguments
+                nodes)
+                    _nodes_tctl_cmd
+                    ;;
                 help)
                    _tctl
                     ;;
             esac
             ;;
     esac
+}
+
+_nodes_tctl_cmd() {
+    local line state
+
+    _arguments -s \
+               "1: :->cmds" \
+               "*::arg:->args"
+
+    case "$state" in
+        cmds)
+            _values "nodes" \
+                    "add[Generate a node invitation token]" \
+                    "ls[List all active SSH nodes within the cluster]"
+            ;;
+        args)
+            case $line[1] in
+                add)
+                    _add_nodes_tctl_cmd
+                    ;;
+                ls)
+                    _ls_nodes_tctl_cmd
+                    ;;
+            esac
+            ;;
+    esac
+}
+
+_ls_nodes_tctl_cmd() {
+    _arguments -s \
+      "--namespace[Namespace of the nodes]:string:"
+}
+
+_users_tctl_cmd() {
+    local line state
+
+    _arguments -s \
+               "1: :->cmds" \
+               "*::arg:->args"
+
+    case "$state" in
+        cmds)
+            _values "users" \
+                    "add[Generate a user invitation token \[Teleport DB users only\]]" \
+                    "ls[List all user accounts]" \
+                    "rm[Deletes user accounts]" \
+                    "reset[Reset user password and generate a new token \[Teleport DB users only\]]" \
+                    "del[Reset user password and generate a new token \[Teleport DB users only\]]"
+            ;;
+        args)
+            case $line[1] in
+                add)
+                    _add_users_tctl_cmd
+                    ;;
+                rm)
+                    _list_users_tctl_cmd
+                    ;;
+                reset)
+                    _list_users_tctl_cmd
+                    ;;
+            esac
+            ;;
+    esac
+}
+
+_list_users_tctl_cmd()
+ {
+  _arguments "*: :( $(tctl users ls | sed -n '3,$p' | awk '{print $1}') )"
+}
+
+_add_users_tctl_cmd() {
+
+    _arguments -s \
+      "--logins[List of allowed SSH logins for the new user]:string:" \
+      "--windows-logins[List of allowed Windows logins for the new user]:string:" \
+      "--roles[List of roles for the new user to assume]:string:" \
+      "--ttl[Set expiration time for token, default is 1h0m0s, maximum is 48h0m0s]:string:"
 }
 
 compdef _tctl tctl
